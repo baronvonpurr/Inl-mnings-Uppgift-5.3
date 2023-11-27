8ce01c184b6e0f69cc7704b2465a16f75c0e9c52
baronvonpurr <robinhood.svensson@hotmail.com>
2023-11-27 21:58:47 +0100

    Add project files.

---
+++ Inlämnings Uppgift 5.3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Uppgift5_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tal = new int[3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"Skriv in tal nr {i + 1}: ");
+                tal[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine($"Summan av talen är: {tal[0] + tal[1] + tal[2]}");
+            Console.ReadKey();
+        }
+    }
+}
